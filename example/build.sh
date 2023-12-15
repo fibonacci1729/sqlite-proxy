5c33bbbb2a9c2e421a2c6962e00af86b3aebfb08
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2023-12-15 13:36:25 +0100

    Update build script
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

--- example/build.sh
+++ example/build.sh
@@ -1,2 +1,3 @@
 cargo component build --release
-wasm-tools compose target/wasm32-wasi/release/example.wasm -d ../sqlite-proxy/target/wasm32-wasi/release/sqlite-proxy.wasm -o service.wasm
+cargo component build --manifest-path ../sqlite-proxy/Cargo.toml --release
+wasm-tools compose target/wasm32-wasi/release/example.wasm -d ../sqlite-proxy/target/wasm32-wasi/release/sqlite_proxy.wasm -o service.wasm

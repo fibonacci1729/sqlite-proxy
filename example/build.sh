7203f30be41baaa30e79a3af11efd69b764f73ce
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2024-02-22 12:44:01 +0100

    Adding Python example
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

--- example/build.sh
+++
@@ -1,3 +0,0 @@
-cargo component build --release
-cargo component build --manifest-path ../sqlite-proxy/Cargo.toml --release
-wasm-tools compose target/wasm32-wasi/release/example.wasm -d ../sqlite-proxy/target/wasm32-wasi/release/sqlite_proxy.wasm -o service.wasm
